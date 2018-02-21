1b2c581b580a891a2028d3f7216b5015dcaff923
Mario Martinez <mart1041@m506-118-26441.local>
2018-02-21 15:52:56 -0800

    Implemented the logout button

--- instagram/AppDelegate.swift
+++ instagram/AppDelegate.swift
@@ -29,7 +29,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             })
         )
         
+        NotificationCenter.default.addObserver(forName: Notification.Name("didLogout"), object: nil, queue: OperationQueue.main) { (Notification) in
+            print("Logout notification received")
+            self.logOut()
+        }
+        
         return true
+    }
+    
+    func logOut() {
+        // Logout the current user
+        PFUser.logOutInBackground(block: { (error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("Successful loggout")
+                // Load and show the login view controller
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let loginViewController = storyboard.instantiateViewController(withIdentifier: "logoutView")
+                self.window?.rootViewController = loginViewController
+            }
+        })
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
